15ffd541479dc4f298e5c7988cd2220261ae5136
Mufti Azan Farooqi <muf.naza@gmail.com>
2022-11-28 10:25:19 +0530

    ci: install prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,23 +1,26 @@
 module.exports = {
   env: {
-    browser: false,
-    es2021: true
+    es2021: true,
+    node: true,
+    commonjs: true,
   },
-  extends: ["eslint:recommended",
-    'plugin:@typescript-eslint/recommended', "plugin:@typescript-eslint/recommended-requiring-type-checking"
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
   ],
   plugins: ["@typescript-eslint"],
-  parser: '@typescript-eslint/parser',
-  overrides: [
-  ],
+  parser: "@typescript-eslint/parser",
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: './tsconfig.json'
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
   },
   rules: {
     "valid-jsdoc": 1,
     "require-jsdoc": 1,
-    "@typescript-eslint/no-explicit-any": 0
-  }
-}
+    "@typescript-eslint/no-explicit-any": 0,
+  },
+};
